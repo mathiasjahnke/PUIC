9329dae52643806558dddef5e8d181fe2d9ee83e
Mathias Jahnke <mjahnke@outlook.com>
2015-03-02 12:46:04 +0100

    added PIComponents interface

---
+++ PUIComponents/src/de/tum/bgu/lfk/puicomponents/button/PIComponent.java
@@ -0,0 +1,36 @@
+package de.tum.bgu.lfk.puicomponents.button;
+
+/**
+ * the components interface
+ * @author Mathias Jahnke, Technische Universit&auml;t M&uuml;nchen, <a href="http://www.lfk.bgu.tum.de">Department of Cartography</a>
+ * @version 0.0.1
+ * @since 02.03.2015
+ *
+ */
+public interface PIComponent {
+	
+	/**
+	 * returns the height of the component
+	 * @return the components height
+	 */
+	public float getComponentHeight();
+	
+	/**
+	 * returns the width of the component
+	 * @return the components width
+	 */
+	public float getComponentWidth();
+	
+	/**
+	 * sets the location of the component
+	 * @param x the x value
+	 * @param y the y value
+	 */
+	public void setLocation(float x, float y);
+	
+	/**
+	 * to draw the component onto the canvas
+	 */
+	public void draw();
+
+}
